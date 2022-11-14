9862280b9168159735ba372c3c68373219564bdf
rimpsh <hi@rimpsh.moe>
2022-11-14 22:40:14 +0000

    Switch from log to trace

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::{io::Result, net::TcpListener};
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use ztpir::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to set logger.");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber.");
 
     let config = get_configuration().expect("Failed to read configuration file");
     let connection_pool = PgPool::connect(&config.database.connection_string())
