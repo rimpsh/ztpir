3accd15868e0ad6f50f81fd0648e9a7195f030bf
rimpsh <hi@rimpsh.moe>
2022-11-14 21:14:44 +0000

    Subscribe with logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::{io::Result, net::TcpListener};
 use ztpir::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("Failed to read configuration file");
     let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::io::Error;
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(db_pool.clone())
